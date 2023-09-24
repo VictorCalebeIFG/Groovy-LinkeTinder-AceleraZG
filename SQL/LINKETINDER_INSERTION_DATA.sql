0ac3df80a230899b84798b2e0b879b424ac7b305
Victor Calebe Cavalcante de Macedo <calebe.m@academico.ifg.edu.br>
2023-09-24 11:30:27 -0300

    SQL: Adicionando default_data (data insertion)

---
+++ SQL/LINKETINDER_INSERTION_DATA.sql
@@ -0,0 +1,24 @@
+INSERT INTO CANDIDATO (NOME, SOBRE_NOME, DATA_NASCIMENTO, EMAIL, CPF, PAIS, CEP, DESC_PESSOAL, SENHA)
+VALUES
+  ('João', 'Silva', '1990-05-15', 'joao@email.com', '123.456.789-01', 'Brasil', '12345-678', 'Descrição pessoal do João', 'senha123'),
+
+  ('Maria', 'Santos', '1985-08-20', 'maria@email.com', '987.654.321-09', 'Brasil', '54321-876', 'Descrição pessoal da Maria', 'senha456'),
+
+  ('Carlos', 'Gomes', '1995-03-10', 'carlos@email.com', '555.444.333-22', 'Brasil', '98765-432', 'Descrição pessoal do Carlos', 'senha789'),
+
+  ('Ana', 'Ferreira', '1980-11-25', 'ana@email.com', '111.222.333-44', 'Brasil', '24680-135', 'Descrição pessoal da Ana', 'senha101'),
+
+  ('Paulo', 'Oliveira', '1998-07-02', 'paulo@email.com', '777.888.999-55', 'Brasil', '86420-579', 'Descrição pessoal do Paulo', 'senha202');
+
+
+INSERT INTO CANDIDATO (NOME, SOBRE_NOME, DATA_NASCIMENTO, EMAIL, CPF, PAIS, CEP, DESC_PESSOAL, SENHA)
+VALUES
+  ('João', 'Silva', '1990-05-15', 'joao@email.com', '123.456.789-01', 'Brasil', '12345-678', 'Descrição pessoal do João', 'senha123'),
+
+  ('Maria', 'Santos', '1985-08-20', 'maria@email.com', '987.654.321-09', 'Brasil', '54321-876', 'Descrição pessoal da Maria', 'senha456'),
+
+  ('Carlos', 'Gomes', '1995-03-10', 'carlos@email.com', '555.444.333-22', 'Brasil', '98765-432', 'Descrição pessoal do Carlos', 'senha789'),
+
+  ('Ana', 'Ferreira', '1980-11-25', 'ana@email.com', '111.222.333-44', 'Brasil', '24680-135', 'Descrição pessoal da Ana', 'senha101'),
+
+  ('Paulo', 'Oliveira', '1998-07-02', 'paulo@email.com', '777.888.999-55', 'Brasil', '86420-579', 'Descrição pessoal do Paulo', 'senha202');
